bb2c61e6533badb2216dd29c5196368f7611f40c
kgodisela <120120470+kgodisela@users.noreply.github.com>
2023-03-31 14:59:56 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,12 +1,9 @@
-<!-- Gainsight PX Tag-->
- 
+
   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-IBPAQL0KHSJ7-2");
-
-<!-- Gainsight PX Tag-->
 
 function allowlogin(usermail)
 {
