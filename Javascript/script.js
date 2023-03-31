b412977007b47418f966af1e40ce0eef34ac3da6
kgodisela <120120470+kgodisela@users.noreply.github.com>
2023-03-31 14:12:32 +0530

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,30 +1,37 @@
-
-
+<!-- Gainsight PX Tag-->
+  <script type="text/javascript">
   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-BOFSTKFNWCGO-2");
-
-
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-IBPAQL0KHSJ7-2");
+</script>
+<!-- Gainsight PX Tag-->
 
 function allowlogin(usermail)
 {
     var a = document.getElementById("usermail").value;
-    if (a == 'demouser@gmail.com' || a == 'lkchittajallu@gainsight.com' || a == 'dpraj@gainsight.com')
+    if (a == 'kgodiselagainsight.com')
     { 
         var b= a.substr(0,5);
         alert('valid user');
-        aptrinsic("identify",
-            {
-
-                "id": b,
-                "email": a
-            },
-            {
-                "id": "GS",
-                "name": "Gainsight"
-            }); 
+        
+      aptrinsic("identify",
+      {
+      //User Fields
+        "id": "unique-user-id", // Required for logged in app users
+        "email": "userEmail@address.com",
+        "firstName": "John",
+        "lastName": "Smith",
+        
+     },
+      {
+      //Account Fields
+        "id":"IBM", //Required
+        "name":"International Business Machine",
+        "Program": "Platinum" // flat custom attributes
+     });
+      
         location.href = "Page1.html";
     }
     else if(a == '')
